d2a7d30ba152b124fbfb28b28b56de2ecf19d10d
Roselynn Conrady <conrady.roselynn@gmail.com>
2021-11-29 16:08:05 -0600

    added builder lines

---
+++ Assets/Scripts/Builder_MuralEffects.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Vuforia;
+using TMPro;
+
+public class Builder_MuralEffects : MonoBehaviour
+{
+    public string[] instructionLines; 
+    public string[] lines; // builder dialogue
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
